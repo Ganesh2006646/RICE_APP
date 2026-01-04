385805d53cb97b5641dbbad19a6e85782a93e0bb
kankatlaganeshgiridar <kankatlaganeshgiridhar@gmail.com>
2026-01-04 23:30:07 +0530

    Release config: Updated App ID, branding, and generated launcher icons

--- mobile/flutter_app/android/app/build.gradle.kts
+++ mobile/flutter_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.rice_agent"
+    namespace = "com.ricemill.agent"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.rice_agent"
+        applicationId = "com.ricemill.agent"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
